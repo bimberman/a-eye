42d1b1b500893a71a2b1c39e7ce0f5bf5ed21f2d
Serin Han <serinushan@gmail.com>
2020-06-24 13:03:15 -0700

    accordion with reactstrap

--- client/components/owned-dogs.jsx
+++ client/components/owned-dogs.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { UncontrolledCollapse, Button, CardBody, Card } from 'reactstrap';
 
 export default class OwnedDogs extends React.Component {
   constructor(props) {
@@ -19,18 +20,40 @@ export default class OwnedDogs extends React.Component {
     const dogs = this.state.ownedDogs.map(dog => {
       return (
         <div key={dog.name}>
-          <p>{dog.name}</p>
-          <p>{dog.breed}</p>
-          <p>{dog.shortDescription}</p>
-        </div>
+          <Button color="primary" id={dog.name} className='w-100 mt-1 d-flex justify-content-between button'>
+            <img src='' />
+            <p>{dog.name}</p>
+            <p>&#9660;</p>
+          </Button>
+          <UncontrolledCollapse toggler={`#${dog.name}`}>
+            <Card>
+              <CardBody>
+                {dog.breed}
+              </CardBody>
+            </Card>
+          </UncontrolledCollapse>
+        </div >
       );
     });
-    return dogs;
+
+    return (
+      <div className='d-flex flex-column justify-content-center w-100'>
+        {dogs}
+      </div>
+    );
   }
 
   render() {
     return this.state.ownedDogs.length > 0
-      ? this.getDogInfo()
+      ? (
+        <div className='container-fluid d-flex justify-content-center flex-wrap align-content-between'>
+          <div className="main-portrait-container col-9">
+            <img src="./images/hello.jpg" alt=""
+              className='rounded-circle img-thumbnail img-fluid' />
+          </div>
+          {this.getDogInfo()}
+        </div>
+      )
       : <h1>No Owned Dogs</h1>;
   }
 }
