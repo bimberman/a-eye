765b5be64081af7f3810b2de2aa808bc66fd6647
Serin Han <serinushan@gmail.com>
2020-06-25 15:22:29 -0700

    Created hook for for long press on accordion item

--- client/components/accordion.jsx
+++ client/components/accordion.jsx
@@ -1,15 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Collapse, Button, CardBody, Card } from 'reactstrap';
 
 export default function Accordion(props) {
+  const [startLongPress, setStartLongPress] = useState(false);
   const [collapse, setCollapse] = useState(false);
   const [status, setStatus] = useState('Closed');
   const onEntered = () => setStatus('Opened');
   const onExited = () => setStatus('Closed');
   const toggle = () => setCollapse(!collapse);
+  const onMouseDown = () => setStartLongPress(true);
+  const onMouseUp = () => setStartLongPress(false);
+  const onTouchStart = () => setStartLongPress(true);
+  const onTouchEnd = () => setStartLongPress(false);
+
+  useEffect(() => {
+    let timerId;
+    if (startLongPress) {
+      timerId = setTimeout(props.callback, 300);
+    }
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [props.callback, 300, startLongPress]);
+
   return (
     <div>
-      <Button color="primary" onClick={toggle} id={props.dogName} className='w-100 mt-4 d-flex justify-content-between button'>
+      <Button
+        color="primary"
+        onMouseDown={onMouseDown}
+        onMouseUp={onMouseUp}
+        onTouchStart={onTouchStart}
+        onTouchEnd={onTouchEnd}
+        onClick={toggle}
+        id={props.dogName}
+        className='w-100 mt-4 d-flex justify-content-between button'
+      >
         <img src={props.imageUrl} height={50} />
         <h4>{props.dogName}</h4>
         {status === 'Opened'
--- client/components/owned-dogs.jsx
+++ client/components/owned-dogs.jsx
@@ -25,16 +25,33 @@ export default class OwnedDogs extends React.Component {
       const capitalizedWords = breedWords.map(word => word[0].toUpperCase() + word.slice(1));
       const capitalizedBreed = capitalizedWords.join(' ');
       return (
-        <Accordion key={dog.name} imageUrl={dog.imageUrl} dogName={dog.name} breedName={capitalizedBreed} shortDescription={dog.shortDescription} />
+        <Accordion
+          key={dog.name}
+          callback={() => {
+            // eslint-disable-next-line no-console
+            console.log('CALLBACK');
+          }}
+          imageUrl={dog.imageUrl}
+          dogName={dog.name}
+          breedName={capitalizedBreed}
+          shortDescription={dog.shortDescription} />
       );
     });
     return dogs;
+  }
+
+  editDogInfo() {
+
   }
 
   render() {
     return this.state.ownedDogs.length > 0
       ? (
         <div className='container-fluid d-flex justify-content-center flex-wrap align-content-between'>
+          <div className="back-to-main p-0 text-left col-12">
+            <i className="fas fa-chevron-left"
+              onClick={() => this.props.changeAppView('main')}></i>
+          </div>
           <div className="main-portrait-container col-9">
             <img src="./images/user-icon.png" alt=""
               className='rounded-circle img-thumbnail img-fluid' />
