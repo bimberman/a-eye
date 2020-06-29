f5e4c12f1c7c1ec36ca1c48903e538fbb71baf62
Ben Imberman <bimberman@gmail.com>
2020-06-29 12:05:41 -0700

    Removed dummy data

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -22,21 +22,7 @@ export default class App extends React.Component {
       view: 'main',
       userId: 1,
       currentBreed: 'Pug',
-      prediction: {
-        confidence: 1,
-        imagePath: '/images/dalmatian.jpg',
-        info: {
-          breedId: 1,
-          historicalUsage: 'Dalmatians have a job description unique among AKC breeds: coach dog. Their traditional occupation was to trot beside horse-drawn coaches, and to guard the horses and rig when otherwise unattended. Dals were alongside the caravans of the Romani people, commonly known as gypsies, during their ceaseless wanderings around Europe. This association with the peripatetic Romani helps explain why Dal origins are so difficult to pin down—as with the gypsies themselves, the world was their home.',
-          imageUrl: '/images/dalmatian.jpg',
-          longDescription: 'The Dalmatian is a distinctively spotted dog; poised and alert; strong, muscular and active; free of shyness; intelligent in expression; symmetrical in outline; and without exaggeration or coarseness. The Dalmatian is capable of great endurance, combined with fair amount of speed. Deviations from the described ideal should be penalized in direct proportion to the degree of the deviation.',
-          name: 'Dalmatian',
-          shortDescription: 'Dalmatians have a white coat with black or liver color spots. They typically standing between 19 and 23 inches at the shoulder.',
-          temperament: 'Reserved and dignified, Dals can be aloof with strangers and are dependable watchdogs. With their preferred humans, Dals are bright, loyal, and loving house dogs. They are strong, active athletes with great stamina—a wonderful partner for runners and hikers.',
-          apiKeyWord: 'dalmatian'
-        },
-        label: 'Dalmatian'
-      }
+      prediction: ''
     };
     this.toggleLoading = this.toggleLoading.bind(this);
     this.changePredictionState = this.changePredictionState.bind(this);
