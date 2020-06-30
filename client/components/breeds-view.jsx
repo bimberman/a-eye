ae0a9072fc69317d689d582b4c3f3a5cf0746ca2
Serin Han <serinushan@gmail.com>
2020-06-30 12:44:17 -0700

    browse breed renders desktop view when large screen

--- client/components/breeds-view.jsx
+++ client/components/breeds-view.jsx
@@ -40,12 +40,45 @@ class BreedsView extends React.Component {
         </div>
       );
     });
-    return <div className="d-flex align-items-center flex-column">
-      <Header pageName="Breeds"/>
-      <div className="col-11">
-        {breedsView}
-      </div>
-    </div>;
+
+    const desktopView = this.state.breeds.map((breed, index) => {
+      const pawprint = index ? <i className="fas fa-paw text-center pb-3"></i> : '';
+      return (
+        <div key={breed.breedId} >
+          <div className='text-center'>
+            {pawprint}
+          </div>
+          < div className='d-flex m-2 align-items-center' >
+            <img src={breed.imageUrl} className='col' />
+            <div className='col'>
+              <p>{breed.name}</p>
+              <div className='text-center'>
+                <p>{breed.shortDescription}</p>
+                <Link className="btn btn-sm btn-light" to="/ViewInfo"
+                  onClick={() => changeCurrentBreed(breed.name)}>
+                  <span>View more</span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+
+    return (
+      <div>
+        <div className="d-flex align-items-center flex-column d-lg-none">
+          <Header pageName="Breeds" />
+          <div className="col-11">
+            {breedsView}
+          </div>
+        </div>
+        <div className='d-none d-lg-block'>
+          <Header />
+          {desktopView}
+        </div>
+      </div >
+    );
   }
 
 }
--- client/components/main-view.jsx
+++ client/components/main-view.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from './header';
 
 class MainView extends React.Component {
 
   render() {
     return (
       <div>
+        <div className='d-none d-lg-block'>
+          <Header />
+        </div>
         <div className={`container-fluid d-flex justify-content-center
       flex-wrap align-content-between d-lg-none`}>
           <div className="main-portrait-container col-9">
@@ -35,31 +39,6 @@ class MainView extends React.Component {
               <p className="p-0 m-0 browse-view">Browse</p>
             </Link>
           </div>
-        </div>
-        <div className='d-none d-lg-block'>
-          <header className='d-flex flex-nowrap align-items-center justify-content-between desktop-header'>
-            <div>
-              <img
-                src="./images/user-icon.png"
-                alt="Muscular gray dog cartoon standing up in front of red shield background"
-                className='rounded-circle img-thumbnail img-fluid header-image m-2'
-              />
-            </div>
-            <div className='d-flex justify-content-around align-items-center col-11'>
-              <Link to='/MyDogs'>
-                <p>My Dogs</p>
-              </Link>
-              <Link to='/Scan'>
-                <p>Scan</p>
-              </Link>
-              <Link to='/Upload'>
-                <p>Upload</p>
-              </Link>
-              <Link to='/Browse'>
-                <p>Browse</p>
-              </Link>
-            </div>
-          </header>
         </div>
       </div>
     );
