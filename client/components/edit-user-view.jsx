dd5c43dfcc5fe583329b4329fa4082cdc971d3e6
Marc Nicdao <nicdaom466@gmail.com>
2020-07-02 15:05:14 -0700

    minor changes

--- client/components/edit-user-view.jsx
+++ client/components/edit-user-view.jsx
@@ -61,9 +61,7 @@ class EditUserView extends React.Component {
         {/* Normal display */}
         <div className="container-fluid d-flex justify-content-center
       flex-wrap align-content-between d-lg-none">
-          <div className="p-0 text-left col-12">
-            <Header pageName="Edit User" />
-          </div>
+          <Header pageName="Edit User" />
           <div
             className={`container-fluid d-flex justify-content-center
           flex-wrap align-content-between`}>
